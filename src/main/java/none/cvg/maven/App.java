98a779b2e2733bbeccfe2cd02b17253c56be2ed8
Zarina <kuzhakova-zarina@mail.ru>
2022-12-05 13:44:59 +0300

    Update App.java

--- src/main/java/none/cvg/maven/App.java
+++ src/main/java/none/cvg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals. (c) Sirius Black" );
     }
 }
